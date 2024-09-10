1475c5a2895d0564751f0e793d95459b1b30b27a
Zency <2659255665@qq.com>
2024-09-10 21:25:20 +0800

    修改了项目部署配置

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-static';
+// import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
